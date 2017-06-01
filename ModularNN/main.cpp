9b968beb171d835bfd3851af60fbdb78519368e5
Hudson <hudsonbr95@gmail.com>
2017-06-01 18:39:33 -0300

    Corrigindo bug da detecção de obstáculos

--- ModularNN/main.cpp
+++ ModularNN/main.cpp
@@ -35,7 +35,7 @@ int main(int argc, char **argv)
 //   float noDetectionDist=0.5;
 //   float maxDetectionDist=0.2;
   float detect[8]={0,0,0,0,0,0,0,0};
-  float v0=2;
+  float v0=0;
   bool fimDeLinha = false;
   
   int clientID=simxStart((simxChar*)serverIP.c_str(),serverPort,true,true,2000,5);
@@ -61,11 +61,11 @@ int main(int argc, char **argv)
       sensorNome[i] = "sensor" + to_string(i + 1);
       
       if(simxGetObjectHandle(clientID,(const simxChar*) sensorNome[i].c_str(),(simxInt *) &sensorHandle[i], (simxInt) simx_opmode_oneshot_wait) != simx_return_ok)
-	cout << "Handle do sensor " << sensorNome[i] << " nao encontrado!" << std::endl;
+            cout << "Handle do sensor " << sensorNome[i] << " nao encontrado!" << std::endl;
       else
       {
         cout << "Conectado ao sensor " << sensorNome[i] << std::endl;
-	simxReadProximitySensor(clientID,sensorHandle[i],NULL,NULL,NULL,NULL,simx_opmode_streaming);
+        simxReadProximitySensor(clientID,sensorHandle[i],NULL,NULL,NULL,NULL,simx_opmode_streaming);
       }
     }
     
@@ -80,38 +80,44 @@ int main(int argc, char **argv)
         vLeft = v0;
         vRight = v0;
         
-        // atualiza velocidades dos motores
-
+        
         simxSetJointTargetVelocity(clientID, leftMotorHandle, (simxFloat) vLeft, simx_opmode_streaming);
         simxSetJointTargetVelocity(clientID, rightMotorHandle, (simxFloat) vRight, simx_opmode_streaming);
-        
+        //if(tempoAtual-tempoAnterior > 10){ 
+        for(int i = 0; i < 8; i++){
+        // atualiza velocidades dos motores        
         simxUChar state;
         simxFloat coord[3];
-        if(tempoAtual-tempoAnterior > 10){
-            for(int i = 0; i < 8; i++){
-                if (simxReadProximitySensor(clientID, sensorHandle[i] ,&state,coord,NULL,NULL,simx_opmode_buffer)==simx_return_ok)
-                {
-                        detect[i] = coord[2];
-                        printf("%f ",detect[i]);
-                                            
+       
+           if(simxReadProximitySensor(clientID, sensorHandle[i] ,&state, coord,NULL,NULL,simx_opmode_buffer)==simx_return_ok){
                         
+                        float dist = coord[2];
+                        
+                        if(state == 0){
+                            dist = 5;
+                        }
+                                 
                         if(myfile.is_open()){
-                                if(detect[i] < 0)
+                                if(dist < 0){
                                     myfile << 0 << ";";    
-                                else
-                                    myfile << detect[i] <<";";    
+                                    cout << 0 << " ";                                            
+                                }
+                                else{
+                                    myfile << dist <<";";    
+                                    cout << dist << " ";                                            
+                                }
                         }
                         
                         
                             if(i+1 == 8){
                                 fimDeLinha = true;
-                                printf("\n");
+                               printf("\n");
                                 myfile <<""<<endl;    
                             } else {
                                 fimDeLinha = false;
                             }
                         
-                    /*   if(detect[i] < 0.1 ){
+                    /*   if(dist < 0.1 ){
                             simxSetJointTargetVelocity(clientID, leftMotorHandle, 0.0, simx_opmode_streaming);
                             simxSetJointTargetVelocity(clientID, rightMotorHandle, 0.0, simx_opmode_streaming);
                             
@@ -126,7 +132,7 @@ int main(int argc, char **argv)
                             simxSetJointTargetVelocity(clientID, rightMotorHandle,(simxFloat) -vRight, simx_opmode_streaming);
                             extApi_sleepMs(250);
                         }   */   
-                    }
+                    //} 
             }
                     tempoAnterior = tempoAtual;
         }
