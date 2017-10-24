9ad776fd683958e29b5d0d12fcbd054464d73227
Pedro Santos <pedrorezendesantos@gmail.com>
2017-10-24 14:33:00 -0200

    EP novas alterações

--- Python VREP/EP.py
+++ Python VREP/EP.py
@@ -3,25 +3,24 @@ from scipy.stats import norm
 import numpy
 #PARÂMETROS DO EP --> EP diferencia-se de AG pelo uso somente de mutação
 class cromossomo():
-    #Funçao de Custo
-    self.custoTotal = 0
-    self.custoS = 0
-    self.custoP = 0
-    self.Q1 = 1
-    self.Q2 = 1
-    self.Q3 = 1
-    self.Q4 = 1
-    self.alpha = 1
-    self.beta = 1
-    self.fitness = 0.0
-
-    self.Col = 0
-    self.Osc = 0
-    self.Lng = 0
-    self.Arr = 0
-    self.Clr = 0 #valores devem vir após teste da solução na simulação
-
     def __init__ (self, pesosIniciais):
+        #Funçao de Custo
+        self.custoTotal = 0
+        self.custoS = 0
+        self.custoP = 0
+        self.Q1 = 1
+        self.Q2 = 1
+        self.Q3 = 1
+        self.Q4 = 1
+        self.alpha = 1
+        self.beta = 1
+        self.fitness = 0.0
+
+        self.Col = 0
+        self.Osc = 0
+        self.Lng = 0
+        self.Arr = 0
+        self.Clr = 0 #valores devem vir após teste da solução na simulação
         self.pesos = pesosIniciais
         self.cromossomo_size = 18
 
@@ -33,43 +32,37 @@ class cromossomo():
                     self.cromossomo.append(peso)
 
 
-    def setFitness(self):
-        self.verificaCusto_S(cromossomo)
-        self.verificaCusto_P(self.Col, self.Osc, self.Lng, self.Arr, self.Clr)
-        self.fitness = (self.alfa*self.custoS) + (self.beta*self.custoP)
+    def setFitness(self, fitness):
+        self.fitness = fitness;
 
     def getFitness(self):
         return self.fitness
 
-    def verificaCusto_P(self):
-        self.custoP = 0
-        self.custoP = self.custoP + (self.Q1*(self.Col*10000) + self.Q2*(self.Osc*0.1) + self.Q3*self.Lng + self.Q4*(self.Arr*100) + self.Q5*(1 - self.Clr))
+    def setParam(self, Col, Osc, Lng, Arr, Clr):
+        self.Col = Col
+        self.Osc = Osc
+        self.Lng = Lng
+        self.Arr = Arr
+        self.Clr = Clr #ok? VIADUMMMM
 
-    def verificaCusto_S(cromossomo):
-        self.custoS = 0
-        for i in range(self.cromossomo_size):
-            if(i < 9):
-                self.custoS = self.custoS + cromossomo[i]
+
 
 class EP:
     def __init__(self, pesosIniciais):
-        self.cromossomo = []
         self.population = []
         self.population_size = 30
-        self.cromossomo_size = 18
         self.generations = 200
         self.elite_size = 3
         self.mutation_rate = 0.05
         self.tournament_size = 15
-        self.fitness = []
-
-
 
         iniciaPopulacao(self.population_size, pesosIniciais)
         #envia pesos pra rede e recebe os valores para calcular o fitness de todos os
-        # Col, Osc, Lng, Arr, Clr =
-        evaluation(self.population)
-
+        # Col, Osc, Lng, Arr, Clr = aqui? criar setParam ?
+        #dita ae como seria lei
+        for cromossomo in self.population:
+            evaluation(cromossomo)
+            #aqui a condição de parada também precisa ser alterada --> ok
         for generation in range(generations):
             print "Generation: "+str(generation)
             #condição de parada
@@ -77,14 +70,16 @@ class EP:
                 if self.fitness[i] <= 80.0:
                     print "Condição de Parada: Fitness"
                     break
-            #mutation
-            #evaluation
+
+            for cromossomo in self.population:
+                #mutation
+                mutation(populacao)
+                #evaluation então, aqui que tem que fazer a integração com a rede
+                cromossomo.setParam(Col, Osc, Lng, Arr, Clr)
+                evaluation(cromossomo)
+
             #selection
-            populacao = mutation(populacao)
-            #envia pesos pra rede e recebe os valores para calcular o fitness de todos os
-            # Col, Osc, Lng, Arr, Clr =
-            calculaFitness(self.population, Col, Osc, Lng, Arr, Clr)
-            populacao = selection(populacao)
+            selection(self.population)
 
 
     def iniciaPopulacao(self, population_size, pesos):
@@ -92,24 +87,31 @@ class EP:
             self.population.append(cromossomo(pesos))
 
 
-    def evaluation(self, Col, Osc, Lng, Arr, Clr):
-        for cromossomo in self.population:
-            getFitness(cromossomo)
+    def evaluation(self, cromossomo): #nova forma?? daqui pra baixo tá minha dúvida, como acha que seria melhor, po
+        self.verificaCusto_S(cromossomo)
+        self.verificaCusto_P(cromossomo)
+        cromossomo.setFitness((self.alfa*self.custoS) + (self.beta*self.custoP))  #certo? então,aí que está esses parametros teriam que já ser atualizados diretamente no cromossomo ok?
 
+    def verificaCusto_P(cromossomo):
+        cromossomo.custoP = 0
+        cromossomo.custoP = cromossomo.custoP + (cromossomo.Q1*(cromossomo.Col*10000) + cromossomo.Q2*(cromossomo.Osc*0.1) + cromossomo.Q3*cromossomo.Lng + cromossomo.Q4*(cromossomo.Arr*100) + cromossomo.Q5*(1 - cromossomo.Clr))
 
-            self.fitness.append()
-
-
+    def verificaCusto_S(cromossomo):
+        cromossomo.custoS = 0
+        for i in range(cromossomo.cromossomo_size):
+            if(i < 9):
+                cromossomo.custoS  = cromossomo.custoS + cromossomo[i]
 
     def selection(self):
         elite = []
-        #Rank-based
+        #Rank-based -- cada cromossomo aqui são os pesos da rede --> redes com pesos diferentes e mesma topologia são rede diferentes, entende?
         rank_based = sorted(self.fitness)
         for i in rang(rank_based):
             if (i < 3):
                 elite = rank_based[i]
             else:
-
+#kkkkk
+ou a mutacao tá foda---> não acho nada e o selection...pearai
 
     def mutation(self):
         for
